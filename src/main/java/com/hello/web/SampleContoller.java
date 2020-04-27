fa5fb86d5493157fdf99200f195d00201eccf396
Tyler Stegman <stegman44@gmail.com>
2020-04-27 18:32:43 -0400

    init commit

---
+++ src/main/java/com/hello/web/SampleContoller.java
@@ -0,0 +1,24 @@
+package com.hello.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hello.business.Pet;
+
+@Controller
+public class SampleContoller {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello Java Bootcamp!";
+	}
+	
+	@RequestMapping("/pet")
+	@ResponseBody
+	public Pet displayPet() {
+		Pet p = new Pet(1, "Clyde", "Cat");
+		return p;
+	}
+}
